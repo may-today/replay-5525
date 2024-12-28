3856da9551a15611bad6f850ea8e397040cec22e
ddiu8081 <ddiu8081@gmail.com>
2024-12-28 13:36:20 +0800

    feat: add normal rain stat

--- app/components/reports/RainStat.tsx
+++ app/components/reports/RainStat.tsx
@@ -5,17 +5,31 @@ import { selectedConcertDateTypeMapAtom, selectedConcertDetailsAtom } from '~/st
 import type { Concert, ConcertSelectType } from '~/data/types'
 import { useFocusValueMap } from '~/hooks/useFocus'
 import { AnimatedNumber } from '~/components/ui/animated-number'
+import ConcertTitle from '../ConcertTitle'
 
-const rainDateList = ['2024.04.30', '2024.05.04', '2024.08.04', '2024.10.04']
+const stormRainDateList = ['2024.04.30', '2024.05.04', '2024.08.04', '2024.10.04']
+const normalRainDateList = ['2024.05.24', '2024.09.11', '2024.11.16']
 
 export const shouldShowRainStat = (
   selectedConcertDetails: Concert[],
   selectedConcertDateTypeMap: Record<string, ConcertSelectType>
 ) => {
-  return selectedConcertDetails.some((concert) => isRainConcert(concert, selectedConcertDateTypeMap))
+  return selectedConcertDetails.some((concert) => isRainConcert(concert, selectedConcertDateTypeMap, 'all'))
 }
 
-const isRainConcert = (concert: Concert, selectedConcertDateTypeMap: Record<string, ConcertSelectType>) => {
+const isRainConcert = (
+  concert: Concert,
+  selectedConcertDateTypeMap: Record<string, ConcertSelectType>,
+  rainType: 'storm' | 'normal' | 'all'
+) => {
+  const rainDateList =
+    rainType === 'all'
+      ? [...stormRainDateList, ...normalRainDateList]
+      : rainType === 'storm'
+        ? stormRainDateList
+        : rainType === 'normal'
+          ? normalRainDateList
+          : []
   return (
     rainDateList.includes(concert.date) &&
     (concert.city === '香港' || selectedConcertDateTypeMap[concert.date] !== 'seats')
@@ -27,11 +41,15 @@ const getPageData = (
   selectedConcertDateTypeMap: Record<string, ConcertSelectType>
 ) => {
   const listenedRainAmount = selectedConcertDetails.filter((concert) =>
-    isRainConcert(concert, selectedConcertDateTypeMap)
+    isRainConcert(concert, selectedConcertDateTypeMap, 'all')
   ).length
+  const listenedStormRainList = selectedConcertDetails.filter((concert) =>
+    isRainConcert(concert, selectedConcertDateTypeMap, 'storm')
+  )
 
   return {
     listenedRainAmount,
+    listenedStormRainList,
   }
 }
 
@@ -46,6 +64,7 @@ const RainStat: React.FC<{ focus: boolean }> = ({ focus }) => {
 
   const animValue = useFocusValueMap(focus, () => ({
     listenedRainAmount: data.listenedRainAmount,
+    listenedStormRainAmount: data.listenedStormRainList.length,
   }))
 
   return (
@@ -58,8 +77,22 @@ const RainStat: React.FC<{ focus: boolean }> = ({ focus }) => {
       <div className="text-report-normal">
         <AnimatedNumber className="text-report-large" value={animValue.listenedRainAmount} />
         <span>场雨</span>
+        {data.listenedStormRainList.length > 0 && (
+          <>
+            <span>，其中</span>
+            <AnimatedNumber className="text-report-large" value={animValue.listenedStormRainAmount} />
+            <span>场暴雨在</span>
+            <div className="mt-2 text-right">
+              {data.listenedStormRainList.map((concert) => (
+                <div key={concert.date}>
+                  <ConcertTitle className="text-report-large" concert={concert} />
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
-      {data.listenedRainAmount === rainDateList.length && (
+      {data.listenedStormRainList.length === stormRainDateList.length && (
         <div className="text-report-normal">
           <span>获得成就</span>
           <span className="text-report-large">暴雨全勤</span>
